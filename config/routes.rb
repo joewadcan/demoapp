5cfea18362f900301ce2b5f5a155fd23ecd5e2a4
joe_wadcan <joe_wadcan@mba.berkeley.edu>
2015-11-13 03:43:26 +0000

    changed the routes to the home/help page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
+  root 'static_pages#home'
+  
+  match 'help', to: 'static_pages#help', via: 'get'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
