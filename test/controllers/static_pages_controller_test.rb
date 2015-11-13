0ffdb98a9bb41a5a24a81c0810c4ed2557f7fa26
joe_wadcan <joe_wadcan@mba.berkeley.edu>
2015-11-13 03:25:57 +0000

    added home/help page with tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "h1", "DemoApp"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "h1", "Help"
   end
 
 end
