c9a87c841e74be5975651bb0214ed8e6b53094ad
Joe Wadcan <joe@wadcan.com>
2018-02-08 05:41:04 +0000

    tweets and tests

---
+++ app/views/tweets/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @tweet, :id, :name, :tweet, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208051532) do
+ActiveRecord::Schema.define(version: 20180208053100) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name"
