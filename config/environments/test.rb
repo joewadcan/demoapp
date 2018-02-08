430cdfa74afb0853f0b4bd0e581ef56660437464
Joe Wadcan <joe@wadcan.com>
2018-02-08 05:53:53 +0000

    updates

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
+  config.serve_static_files  = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
@@ -30,7 +30,10 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
+  
+  config.active_support.test_order = :sorted
+  #because it said so
+  
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
